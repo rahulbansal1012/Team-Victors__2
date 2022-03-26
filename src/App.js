fc0f3b233023f5304deadc997107723ebffd3b84
Rahul Bansal <bansalrahul1012@gmail.com>
2022-03-26 22:56:31 +0530

    this is secong commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   <h1 className="logo">
+          <a href="#">Terence <span>Devine</span></a>
+        </h1>
+  <div className= "rahul">
+
+  
+        <div className="nav-wrap">
+          <nav className="main-nav" role="navigation">
+            <ul className="unstyled list-hover-slide">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Orders</a></li>
+              <li><a href="#">Payment</a></li>
+              <li><a href="#">support</a></li>
+            </ul>
+          </nav>
+          <ul className="social-links list-inline unstyled list-hover-slide">
+           
+            <li><a href="#">Login/Sign up
+
+            </a></li>
+           
+          </ul>
+          
+        </div>
+        <div className = "Rahul">
+          rahul bansal
+        </div>
+    <div className= 'box'>
+      <h1> This is Box1</h1>
+      
     </div>
+        </div>
+   
+   </>
+   
   );
 }
 
